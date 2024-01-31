5cd3a46c83fa0786bd4a9db117c47362ed25d901
John Angwin <john.angwin@gmail.com>
2024-01-30 16:29:24 -0800

    Update addresses for new base.

--- src/steps/create-pools.ts
+++ src/steps/create-pools.ts
@@ -7,38 +7,38 @@ const FEE_TIER = 500
 const POOLS = [
   {
     name: 'USDC / WBTC',
+    token0: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+    token1: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+    feeTier: FEE_TIER,
+  },
+  {
+    name: 'USDC / WETH',
+    token0: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+    token1: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
+    feeTier: FEE_TIER,
+  },
+  {
+    name: 'USDC / WMATIC',
+    token0: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+    token1: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
+    feeTier: FEE_TIER,
+  },
+  {
+    name: 'WBTC / WETH',
     token0: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
     token1: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
     feeTier: FEE_TIER,
   },
   {
-    name: 'USDC / WETH',
-    token0: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
-    token1: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
-    feeTier: FEE_TIER,
-  },
-  {
-    name: 'USDC / WMATIC',
-    token0: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
-    token1: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
-    feeTier: FEE_TIER,
-  },
-  {
-    name: 'WBTC / WETH',
-    token0: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
-    token1: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
-    feeTier: FEE_TIER,
-  },
-  {
     name: 'WBTC / WMATIC',
-    token0: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
-    token1: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
+    token0: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+    token1: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
     feeTier: FEE_TIER,
   },
   {
     name: 'WETH / WMATIC',
-    token0: '0x0165878A594ca255338adfa4d48449f69242Eb8F',
-    token1: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
+    token0: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
+    token1: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853',
     feeTier: FEE_TIER,
   }
 ];
@@ -57,13 +57,18 @@ export const CREATE_POOLS: MigrationStep = async (state, { signer, gasPrice }) =
 
   const results = [];
   for (const pool of POOLS) {
+    // Simulate the transaction and get the return value
+    const poolAddress = await v3CoreFactory.callStatic.createPool(pool.token0, pool.token1, pool.feeTier, { gasPrice });
+
     // Execute the createPool function for each pool in the list
     const tx = await v3CoreFactory.createPool(pool.token0, pool.token1, pool.feeTier, { gasPrice });
+    await tx.wait();
 
     // Push the result of each iteration to the results array
     results.push({
       message: `UniswapV3Factory added a pool for ${pool.name}`,
-      hash: tx.hash,
+      poolAddress: poolAddress,
+      hash: tx.hash
     });
   }
 
